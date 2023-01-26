a3c849b03bfa6b729790da9bc3cc203a140246e2
shivam <shivam.spirite3@outlook.com>
2023-01-26 14:44:09 +0530

    add example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
